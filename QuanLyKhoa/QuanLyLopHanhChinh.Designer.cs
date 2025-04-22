21151922da8344ff74db4e3aa6e5e0a1ea30c141
tuanc62k5 <dinhtuananh.dev@gmail.com>
2025-04-22 23:23:27 +0700

    Cập nhật QuanLyLopHanhChinh

---
+++ QuanLyKhoa/QuanLyLopHanhChinh.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace QuanLyKhoa
+{
+    partial class QuanLyLopHanhChinh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboNganh = new System.Windows.Forms.ComboBox();
+            this.cboLop = new System.Windows.Forms.ComboBox();
+            this.txtChuNhiem = new System.Windows.Forms.TextBox();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgvUsers = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(468, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Lớp:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ngành:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(43, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Chủ nhiệm:";
+            // 
+            // cboNganh
+            // 
+            this.cboNganh.FormattingEnabled = true;
+            this.cboNganh.Location = new System.Drawing.Point(122, 23);
+            this.cboNganh.Name = "cboNganh";
+            this.cboNganh.Size = new System.Drawing.Size(301, 24);
+            this.cboNganh.TabIndex = 8;
+            this.cboNganh.SelectedIndexChanged += new System.EventHandler(this.cboNganh_SelectedIndexChanged);
+            // 
+            // cboLop
+            // 
+            this.cboLop.FormattingEnabled = true;
+            this.cboLop.Location = new System.Drawing.Point(531, 23);
+            this.cboLop.Name = "cboLop";
+            this.cboLop.Size = new System.Drawing.Size(280, 24);
+            this.cboLop.TabIndex = 9;
+            this.cboLop.SelectedIndexChanged += new System.EventHandler(this.cboLop_SelectedIndexChanged);
+            // 
+            // txtChuNhiem
+            // 
+            this.txtChuNhiem.Location = new System.Drawing.Point(148, 76);
+            this.txtChuNhiem.Name = "txtChuNhiem";
+            this.txtChuNhiem.Size = new System.Drawing.Size(275, 22);
+            this.txtChuNhiem.TabIndex = 10;
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddNew.Location = new System.Drawing.Point(181, 135);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(87, 61);
+            this.btnAddNew.TabIndex = 11;
+            this.btnAddNew.Text = "Thêm";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(514, 135);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(87, 61);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "Lưu";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(403, 135);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(87, 61);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(293, 135);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(87, 61);
+            this.btnEdit.TabIndex = 14;
+            this.btnEdit.Text = "Sửa";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(624, 135);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(87, 61);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dgvUsers);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 234);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(874, 366);
+            this.panel1.TabIndex = 16;
+            // 
+            // dgvUsers
+            // 
+            this.dgvUsers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvUsers.Location = new System.Drawing.Point(0, 0);
+            this.dgvUsers.Name = "dgvUsers";
+            this.dgvUsers.RowHeadersWidth = 51;
+            this.dgvUsers.RowTemplate.Height = 24;
+            this.dgvUsers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvUsers.Size = new System.Drawing.Size(874, 366);
+            this.dgvUsers.TabIndex = 0;
+            this.dgvUsers.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUsers_CellEnter);
+            // 
+            // QuanLyLopHanhChinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 600);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.txtChuNhiem);
+            this.Controls.Add(this.cboLop);
+            this.Controls.Add(this.cboNganh);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "QuanLyLopHanhChinh";
+            this.Text = "QuanLyLopHanhChinh";
+            this.Load += new System.EventHandler(this.QuanLyLopHanhChinh_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboNganh;
+        private System.Windows.Forms.ComboBox cboLop;
+        private System.Windows.Forms.TextBox txtChuNhiem;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dgvUsers;
+    }
+}
---
+++ QuanLyKhoa/QuanLyLopHanhChinh.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhoa
+{
+    public partial class QuanLyLopHanhChinh : Form
+    {
+        DBservices db = new DBservices();
+        private bool AddNew = false;
+        private bool LoadingData = false;
+        public QuanLyLopHanhChinh()
+        {
+            InitializeComponent();
+        }
+
+        private void QuanLyLopHanhChinh_Load(object sender, EventArgs e)
+        {
+            loadNganh();
+            setEnable(false);
+        }
+        private void loadNganh()
+        {
+            LoadingData = true;
+            string sql = "SELECT * FROM tblNganh";
+            DataTable dt = db.GetData(sql);
+
+            DataRow row = dt.NewRow();
+            row["NG_ID"] = 0;
+            row["NG_TenNganh"] = "Tất cả Nganh";
+            dt.Rows.InsertAt(row, 0);
+
+            cboNganh.DataSource = dt;
+            cboNganh.DisplayMember = "NG_TenNganh";
+            cboNganh.ValueMember = "NG_ID";
+
+            if (cboNganh.SelectedIndex == -1)
+            {
+                cboNganh.SelectedIndex = 0;
+            }
+            LoadingData = false;
+            LoadLopTheoNganh(0);
+        }
+        private void LoadLopTheoNganh(int NG_ID)
+        {
+            if (LoadingData) return;
+            string sql = NG_ID == 0 ? "SELECT * FROM tblLopHanhChinh" : $"SELECT * FROM tblLopHanhChinh WHERE NG_ID = {NG_ID}";
+            DataTable dt = db.GetData(sql);
+
+            cboLop.SelectedIndexChanged -= cboLop_SelectedIndexChanged;
+
+            cboLop.DataSource = dt;
+            cboLop.DisplayMember = "LP_TenLop";
+            cboLop.ValueMember = "LP_ID";
+
+            cboLop.SelectedIndexChanged += cboLop_SelectedIndexChanged;
+
+            this.BeginInvoke(new Action(() =>
+            {
+                dgvUsers.DataSource = null;
+                dgvUsers.DataSource = dt;
+            }));
+            if (int.TryParse(cboNganh.SelectedValue?.ToString(), out int selectNganh) && selectNganh == 0) return;
+        }
+        private void cboNganh_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (LoadingData || cboNganh.SelectedValue == null || cboNganh.SelectedValue is DataRowView) return;
+            if (int.TryParse(cboNganh.SelectedValue.ToString(), out int NG_ID))
+            {
+                LoadLopTheoNganh(NG_ID);
+            }
+        }
+        private void cboLop_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (LoadingData || cboLop.SelectedValue == null || cboLop.SelectedValue is DataRowView) return;
+            if (int.TryParse(cboLop.SelectedValue.ToString(), out int LP_ID))
+            {
+                this.BeginInvoke(new Action(() =>
+                {
+                    string sql = $"SELECT * FROM tblLopHanhChinh WHERE LP_ID = {LP_ID}";
+                    DataTable dt = db.GetData(sql);
+                    dgvUsers.DataSource = null;
+                    dgvUsers.DataSource = dt;
+                }));
+            }
+        }
+        private void dgvUsers_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            if (LoadingData) return;
+            int i = e.RowIndex;
+            if (i >= 0 && dgvUsers.Rows[i].Cells["LP_ID"].Value != null)
+            {
+                LoadingData = true;
+                cboLop.SelectedValue = dgvUsers.Rows[i].Cells["LP_ID"].Value;
+                if (int.TryParse(cboNganh.SelectedValue?.ToString(), out int selectNganhID) && selectNganhID != 0)
+                {
+                    cboLop.SelectedValue = dgvUsers.Rows[i].Cells["NG_ID"].Value;
+                }
+                txtChuNhiem.Text = dgvUsers.Rows[i].Cells["LP_TenChuNhiem"].Value.ToString();
+                LoadingData = false;
+            }
+        }
+        private void setEnable(bool enable)
+        {
+            cboNganh.Enabled = true;
+            cboLop.Enabled = true;
+            txtChuNhiem.Enabled = enable;
+            btnAddNew.Enabled = !enable;
+            btnEdit.Enabled = !enable;
+            btnDelete.Enabled = !enable;
+            btnSave.Enabled = enable;
+            btnExit.Enabled = !enable;
+
+            if (enable && AddNew)
+            {
+                cboLop.Text = "";
+                cboNganh.SelectedIndex = 0;
+                txtChuNhiem.Text = "";
+            }
+        }
+
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            AddNew = true;
+            cboLop.DropDownStyle = ComboBoxStyle.DropDown;
+            cboLop.Text = "";
+            txtChuNhiem.Text = "";
+            setEnable(true);
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            AddNew = false;
+            cboLop.DropDownStyle = ComboBoxStyle.DropDownList;
+            setEnable(true);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string lop = cboLop.Text.Trim();
+            string cn = txtChuNhiem.Text.Trim();
+            string nganh = cboNganh.SelectedValue?.ToString();
+            LoadingData = true;
+            if (AddNew)
+            {
+                string sql = $"INSERT INTO tblLopHanhChinh (LP_TenLop, LP_TenChuNhiem, NG_ID) VALUES (N'{lop}', N'{cn}', N'{nganh}')";
+                db.runQuery(sql);
+                AddNew = false;
+            }
+            else
+            {
+                string id = cboLop.SelectedValue?.ToString();
+                string sql = $"UPDATE tblLopHanhChinh SET LP_TenLop=N'{lop}', LP_TenChuNhiem=N'{cn}', NG_ID=N'{nganh}' WHERE LP_ID=N'{id}'";
+                db.runQuery(sql);
+            }
+            if (int.TryParse(nganh, out int NG_ID))
+                LoadLopTheoNganh(NG_ID);
+
+            setEnable(false);
+            LoadingData = false;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string id = cboLop.SelectedValue?.ToString();
+            if (string.IsNullOrEmpty(id)) return;
+
+            string sql = $"DELETE FROM tblLopHanhChinh WHERE LP_ID=N'{id}'";
+            db.runQuery(sql);
+
+            if (int.TryParse(cboLop.SelectedValue.ToString(), out int LP_ID))
+                LoadLopTheoNganh(LP_ID);
+
+            dgvUsers.DataSource = null;
+            setEnable(false);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
